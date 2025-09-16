b179b6019cb3228e522a771c2ddd0993ce8f33a7
armaanepiic <armaanepiicfb17@gmail.com>
2025-09-16 19:07:47 +0600

    - two different useEffects - two counter

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,30 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  // let count = 0;
-  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
+  const [count2, setcount2] = useState(10);
 
-  useEffect(() => {
-    console.log("Count updated:", count);
-  }, [count]); // Runs whenever count changes
+  // useEffect-1
+  useEffect(()=>{
+    console.log("inside useEffect 1");
+  }, [count1])
+
+  // useEffect-2
+  useEffect(()=>{
+    console.log("inside useEffect 2");
+  }, [count2])
 
   const increase = () => {
-    setCount((count) => count + 1);
-    // console.log(count);
+    setCount1((count1) => count1 + 1);
   };
   const decrease = () => {
-    setCount((count) => count - 1);
-    // console.log(count);
+    setcount2((count2) => count2 - 1);
   };
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 text-center">
       <Header />
-      <Display currentCount={count} />
+      <Display count1={count1} count2={count2} />
       <CounterButtons increment={increase} decrement={decrease} />
     </div>
   );
