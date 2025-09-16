7ca33263f17ce6a34c14fa07127c7ccebc68b692
armaanepiic <armaanepiicfb17@gmail.com>
2025-09-16 19:06:50 +0600

    - creater two counter value

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,7 +1,9 @@
-
-const Display = ({currentCount}) => {
+const Display = ({ count1, count2 }) => {
   return (
-    <div className="text-4xl font-bold text-gray-700">{currentCount}</div>
-  )
-}
+    <div className="flex space-x-5 justify-center">
+      <div className="text-4xl font-bold text-gray-700">{count1}</div>
+      <div className="text-4xl font-bold text-gray-700">{count2}</div>
+    </div>
+  );
+};
 export default Display;
