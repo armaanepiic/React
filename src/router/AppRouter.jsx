fb01d834c1e213ea6aafafce4cc1b69c55ea24d8
armaanepiic <armaanepiicfb17@gmail.com>
2025-09-19 20:06:35 +0600

    - add react router - configure router - add different pages - add routes - index route - link and useNavigate hook

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "../App";
 import About from "../pages/About";
+import Login from "../pages/Login";
+import NotFound from "../pages/NotFound";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route index element={<App />} />
         <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
