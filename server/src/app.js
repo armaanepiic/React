1f35ec8e33d4ecaf7683ee8d4e768e7d081385eb
armaanepiic <armaanepiicfb17@gmail.com>
2025-09-21 21:57:31 +0600

    - nodemone installed - type.module applied - import method applied

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,18 @@
-const express = require('express');
-
-const { hello } = require("./test");
+import express from 'express';
+import { hello } from "./test.js"
 
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/test", (req, res) => {
+  hello();
+  res.send("Changed test!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
