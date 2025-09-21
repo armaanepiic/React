18571b094966045c28475c4e03564c1b1c71e89b
armaanepiic <armaanepiicfb17@gmail.com>
2025-09-21 12:03:41 +0600

    react-router completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,12 +21,13 @@ export default function Header() {
           <button
             onClick={toggletheme}
             className={clsx(
-              "px-4 py-2 rounded-lg font-medium transition-colors duration-300 cursor-pointer",
+              "px-2 py-1 rounded-full transition-colors duration-300 cursor-pointer",
               theme === "light" && "bg-gray-800 text-white hover:bg-gray-700",
               theme === "dark" && "bg-white text-gray-800 hover:bg-gray-100"
             )}
           >
-            {theme === "light" ? "🌙 Dark" : "☀️ Light"}
+            <i className={theme === "light" ? "fas fa-moon" : "fas fa-sun"}></i>
+            {/* {theme === "light" ? " Dark" : " Light"} */}
           </button>
         </div>
       </div>
